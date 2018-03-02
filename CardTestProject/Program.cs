ce3e4ed3d058436583b22ce73d8b6ea2b70a841a
matthiasdurivet <Matthias.durivet@student.howest.be>
2018-03-02 09:08:36 +0100

    Add project files.

---
+++ CardTestProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CardTestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
